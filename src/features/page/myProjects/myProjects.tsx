ed982131a9bedf298679212eb40fdc9cd0cc3a50
LuisEnrDiaz <luis.enr.diaz.garcia@gmail.com>
2023-03-23 14:13:49 +0100

    📦 add card projects

--- src/features/page/myProjects/myProjects.tsx
+++ src/features/page/myProjects/myProjects.tsx
@@ -29,13 +29,33 @@ export function MyProjects() {
 
                 <Card
                     image="./assets/myProjects/todoList.png"
-                    title="todo-list"
+                    title="todo list"
                     description="Seguro que tienes que ir hacer la compra y no tienes un boligrafo y papel no te preocupes da click al enlace y podras tener tu lista."
                     flexRow={true}
                     webLink="https://todo-list-reactts.netlify.app/"
                     codeLink="https://github.com/LuisEnrDiaz/todo-list"
                     front={true}
                 />
+
+                <Card
+                    image="./assets/myProjects/construction.jpg"
+                    title="Tatoo Center Back"
+                    description="Api creada de tattoo center podras obtener tanto tatuajes como usuarios, los usuarios puedes registrarse y hacer login,crear tatuajes, actualizarlos y eliminarlos"
+                    flexRow={false}
+                    codeLink="https://github.com/LuisEnrDiaz/Tattoo-Center-Back"
+                    webLink=""
+                    front={false}
+                ></Card>
+
+                <Card
+                    image="./assets/myProjects/tattooCenter.png"
+                    title="Tatoo Center"
+                    description="Llamadondo a la api creada de tattoo center podras obtener tanto tatuajes como usuarios, los usuarios puedes registrarse y hacer login,crear tatuajes, actualizarlos y eliminarlos"
+                    flexRow={true}
+                    codeLink="https://github.com/LuisEnrDiaz/Tattoo-Center-Front"
+                    webLink="https://tattoo-center.netlify.app/"
+                    front={true}
+                ></Card>
             </div>
         </main>
     );
